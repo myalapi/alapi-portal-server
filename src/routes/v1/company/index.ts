ef7f111d86f5acaca8e422a27540cf4576ad7236
myAlapi <myalapi2022@gmail.com>
2023-01-12 22:18:52 +0530

    Platfomr Error removed

--- src/routes/v1/company/index.ts
+++ src/routes/v1/company/index.ts
@@ -8,41 +8,55 @@ const router = Router();
 
 router.get("/", authMiddle, async (req, res) => {
   const merchants = req.body.user.merchants;
-  const merchs = await Merchants.find(
-    { _id: { $in: merchants } },
-    "merchantId merchantName platforms createdOn"
-  );
-  const finalMerchs = [];
-  for (var i = 0; i < merchs.length; i++) {
-    const m: any = merchs[i];
-    finalMerchs.push({
-      id: m.mechantId,
-      name: m.merchantName,
-      createdOn: m.createdOn,
-      platforms: Object.keys(m.platforms),
-    });
+  try {
+    const merchs = await Merchants.find(
+      { _id: { $in: merchants } },
+      "merchantId merchantName platforms createdOn"
+    );
+    const finalMerchs = [];
+    for (var i = 0; i < merchs.length; i++) {
+      const m: any = merchs[i];
+      finalMerchs.push({
+        id: m.mechantId,
+        name: m.merchantName,
+        createdOn: m.createdOn,
+        platforms: m.platforms?Object.keys(m.platforms):null,
+      });
+    }
+    res.send({ companies: finalMerchs });
+  } catch (error) {
+    console.log(error);
+    
+    res.send({ success: false, error: error });
   }
-  res.send({ companies: finalMerchs });
 });
 
 router.get("/search", authMiddle, async (req, res) => {
   const merchants = req.body.user.merchants;
-  const query = req.query.search===undefined ? "":req.query.search;  
-  const merchs = await Merchants.find(
-    { merchantName: { $regex: query, $options: "i" }, _id: { $in: merchants } },
-    "merchantId merchantName platforms createdOn"
-  );
-  const finalMerchs = [];
-  for (var i = 0; i < merchs.length; i++) {
-    const m: any = merchs[i];
-    finalMerchs.push({
-      id: m.mechantId,
-      name: m.merchantName,
-      createdOn: m.createdOn,
-      platforms: Object.keys(m.platforms),
-    });
+  const query = req.query.search === undefined ? "" : req.query.search;
+  try {
+    const merchs = await Merchants.find(
+      {
+        merchantName: { $regex: query, $options: "i" },
+        _id: { $in: merchants },
+      },
+      "merchantId merchantName platforms createdOn"
+    );
+    const finalMerchs = [];
+    for (var i = 0; i < merchs.length; i++) {
+      const m: any = merchs[i];
+      finalMerchs.push({
+        id: m.mechantId,
+        name: m.merchantName,
+        createdOn: m.createdOn,
+        platforms: m.platforms?Object.keys(m.platforms):null,
+      });
+    }
+    res.send({ companies: merchs });
+  } catch (error) {
+    console.log(error);
+    res.send({ success: false, error: error });
   }
-  res.send({ companies: merchs });
 });
 
 module.exports = router;
