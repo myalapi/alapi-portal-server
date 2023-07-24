66318d6c8834d9f271b61074ea6158eab2b86819
Pulkit0729 <myalapi2022@gmail.com>
2023-07-24 12:02:38 +0530

    merchant model

--- src/models/merchant.ts
+++ src/models/merchant.ts
@@ -1,13 +1,24 @@
 import mongoose from "mongoose";
+enum exportStatusEnum {
+  In_Progress = "In Progress",
+  Completed = "Completed",
+  Error = "Error"
+}
+const ExportSchema = new mongoose.Schema({
+  date: Date,
+  status: { type: String, enum: exportStatusEnum },
+});
 
 const MerchantSchema = new mongoose.Schema({
   merchantName: String,
   merchantEmail: String,
   userId: String,
   createdOn: Date,
+  exportDate: Date,
+  export: ExportSchema,
   platforms: Object,
   link: String,
 });
 
-const Merchant = mongoose.model("Merchant", MerchantSchema, "merchants");
-export default Merchant; 
+const Merchant = mongoose.model("Merchants", MerchantSchema, "merchants");
+export default Merchant;
