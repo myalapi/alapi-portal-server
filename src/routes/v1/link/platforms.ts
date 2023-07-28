fc3bba64191bd559ce84cf9760d72eb15fd4dd08
Shubham Kumar <66840922+Shubham5213@users.noreply.github.com>
2023-07-28 15:25:57 +0530

    added getMerchantPlatformOrganizations API and createSelectedOrganization API

--- src/routes/v1/link/platforms.ts
+++ src/routes/v1/link/platforms.ts
@@ -6,6 +6,7 @@ import IP from "ip";
 import { getMerchant } from "../../../dal/merchant";
 import { getUser } from "../../../dal/user";
 import { getPlatformByKey } from "../../../dal/platform";
+import platformKeys from "../../../constant/platformKeys";
 
 const router = Router();
 
@@ -140,6 +141,110 @@ router.post("/:platformKey", async (req, res) => {
     });
     return res.send({ sucess: false, msg: error.message });
   }
+});
+
+// set selected orgainsation in platform
+router.post("/:platformKey/org", async (req, res) => {
+  const merchantId = req.body.merchantId;
+  const organizationId = req.body.organization;
+  const platformKey = req.params.platformKey;
+
+  try {
+    if (
+      platformKey === undefined ||
+      platformKey === null ||
+      merchantId === undefined ||
+      merchantId === null ||
+      organizationId === undefined ||
+      organizationId === null
+    ) {
+      throw new Error("Invalid Request"); 
+    } else {
+      const merchant: any = await getMerchant(merchantId);
+      let platforms: any = merchant.platforms;
+
+      if(platforms === null || platforms===undefined) {
+        throw new Error("No Platforms found");  
+      }
+
+      const platform:any = platforms[platformKey];
+      platform.selectedOrganizationId = organizationId;
+      platforms[platformKey] = platform;
+
+      merchant.platforms = platforms;
+      merchant.markModified("platforms");
+      await merchant.save();
+
+      logger.log({
+      level: "info",
+      message: `create Selected Organization Api, ip: ${IP.address()} merchantId: ${merchantId} 
+      platformKey: ${platformKey} organizationId: ${organizationId} URL: ${
+        req.protocol
+      }://${req.get("host")}${req.originalUrl}`,
+    });
+      return res.send({success:true, msg: "Merchant saved"}); 
+    }
+  } catch (error: any) {
+    logger.log({
+      level: "error",
+      message: `create Selected Organization Api, ip: ${IP.address()} error: ${
+        error.message
+      } merchantId: ${merchantId} platformKey: ${platformKey} organizationId: ${organizationId} URL: ${
+        req.protocol
+      }://${req.get("host")}${req.originalUrl}`,
+    });
+    return res.send({ success: false, msg: error.message });
+  }
+});
+
+// Get merchant zoho organisations
+router.post("/:platformkey/info", async (req, res) => {
+  const platformKey= req.params.platformkey;
+  if(platformKey ===platformKeys.zoho){
+      const merchantId = req.body.merchantId;
+    try {
+      if (merchantId === null || merchantId === undefined) {
+        return res
+          .status(401)
+          .json({ success: false, message: "merchantId required" });
+      }
+
+      const merchant: any = await getMerchant(merchantId);
+      if (!merchant) throw new Error(" Merchant not found");
+
+      const platforms:any = merchant.platforms;
+      if(platforms===null || platforms===undefined) 
+          throw new Error("No Platforms found");
+
+      const organizations: { id: any; name: any; }[]= platforms[platformKey].organizations;
+
+      if(organizations===null || organizations===undefined) 
+          throw new Error("No Organizations found");
+      
+      logger.log({
+        level: "info",
+        message: `Link portal: get Platform zoho organisations, ip: ${IP.address()} merchantId: ${merchantId}
+        } URL: ${req.protocol}://${req.get("host")}${req.originalUrl}`,
+      });
+      return res.status(200).json({
+        success: true,
+        organizations,
+      });
+    } catch (error: any) {
+      logger.log({
+        level: "error",
+        message: `Link portal: get Platform zoho organisations, ip: ${IP.address()} error: ${
+          error.message
+        } merchantId:${merchantId} URL: ${req.protocol}://${req.get("host")}${
+          req.originalUrl
+        }`,
+      });
+      return res.send({ success: false, message: error.message });
+    }
+  }else{
+    return res.send(201);
+  }
+
 });
 
 module.exports = router;
